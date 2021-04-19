2ca3fb312acc367166235cc1a0c4ccf0948110ad
Tandangle <tandangle76@gmail.com>
2021-04-19 10:21:06 -0400

    added player draft info, changed styling, added youtube hightlights link

--- src/components/PlayerInfo.js
+++ src/components/PlayerInfo.js
@@ -4,27 +4,43 @@ import { PlayerContext } from "./PlayerContext";
 function PlayerInfo(props) {
 
     return (
-        props.player &&  
+        props.player ? 
             <div className="playerInfo">
                 <img className="playerImage" src={"https://www.basketball-reference.com/req/202104152/images/players/" + props.player + ".jpg"}/>
-                <div className="playerName">
-                    Player Name: {props.salaries.find(e => e.Player_Code === props.player)?.Player}
+                <div className="playerHeader">
+                    <div className="playerName">
+                        Player Name: {props.salaries.find(e => e.Player_Code === props.player)?.Player}
+                    </div>
+                    <div className="playerTeam">
+                        Team: {props.teams[props.salaries.find(e => e.Player_Code === props.player)?.Tm]}
+                    </div>
+                    <div className="playerDraft">
+                        Draft Class of {props.draft.find(e => e.Player_Code === props.player)?.Year} - Round {props.draft.find(e => e.Player_Code === props.player)?.Rd}, Pick {props.draft.find(e => e.Player_Code === props.player)?.Pk}
+                    </div>
                 </div>
-                <div className="playerTeam">
-                    Team: {props.teams[props.salaries.find(e => e.Player_Code === props.player)?.Tm]}
+                <div className="playerStats">
+                    <div className="playerBox">
+                        Box Plus Minus: {props.stats.find(e => e.Player_Code === props.player)?.BPM ? props.stats.find(e => e.Player_Code === props.player)?.BPM : "No BPM stat found"}
+                    </div>
+                    <div className="playerSalary">
+                        2021 - 2022 Salary: ${props.salaries.find(e => e.Player_Code === props.player)?.current_salary.replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                    </div>
+                    <div className="bpmPerMillion">
+                    {props.stats.find(e => e.Player_Code === props.player)?.BPM && props.salaries.find(e => e.Player_Code === props.player)?.current_salary ? 
+                    "Cost per 1 BPM: $"  + (props.salaries.find(e => e.Player_Code === props.player)?.current_salary / props.stats.find(e => e.Player_Code === props.player)?.BPM).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")  : 
+                    "Insufficient stats available to calculate cost per BPM"}
+                    </div>
                 </div>
-                <div className="playerBox">
-                    Box Plus Minus: {props.stats.find(e => e.Player_Code === props.player)?.BPM ? props.stats.find(e => e.Player_Code === props.player)?.BPM : "No BPM stat found"}
-                </div>
-                <div className="playerSalary">
-                    2021 - 2022 Salary: ${props.salaries.find(e => e.Player_Code === props.player)?.current_salary.replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-                </div>
-                <div className="bpmPerMillion">
-                {props.stats.find(e => e.Player_Code === props.player)?.BPM && props.salaries.find(e => e.Player_Code === props.player)?.current_salary ? 
-                "Cost per 1 BPM: $"  + (props.salaries.find(e => e.Player_Code === props.player)?.current_salary / props.stats.find(e => e.Player_Code === props.player)?.BPM).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")  : 
-                "Insufficient stats available to calculate cost per BPM"}
-                </div>
-            </div>
+               <div className="playerLinks">
+                    <div className="playerLink">
+                        <a href={"https://www.basketball-reference.com/players/" + props.player.slice(0,1) + "/" + props.player + ".html"} target="_blank">Link to {props.salaries.find(e => e.Player_Code === props.player)?.Player}'s Basketball Reference page</a>
+                    </div>
+                    <div className="playerYoutube">
+                        <a href={"https://www.youtube.com/results?search_query=" + props.salaries.find(e => e.Player_Code === props.player)?.Player.split(" ")[0] + "+" + props.salaries.find(e => e.Player_Code === props.player)?.Player.split(" ")[1] + "+" + "highlights"} target="_blank">Link to {props.salaries.find(e => e.Player_Code === props.player)?.Player}'s Highlights on Youtube</a>
+                    </div>
+               </div>
+               
+            </div> : <div className="playerInfo"></div>
     );
   }
   
